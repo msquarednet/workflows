6ff75af4bdfc8cc23c862cccf28f690e0f59b575
msquarednet <msquarednet@gmail.com>
2015-02-11 11:04:49 -0500

    Installed gulp and gulp-util

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+var gutil= require('gulp-util');
+
+
+//gulp.task('somenameofyourchoice', fn);
+
+gulp.task('log', function() {
+	gutil.log('Workflows are swell.');
+});
