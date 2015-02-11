e5a2cbd20fd9bc3e14c8ab06c2961aaba42ef57f
msquarednet <msquarednet@gmail.com>
2015-02-11 12:03:05 -0500

    Added CoffeeScript task to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
 var gulp = require('gulp');
 var gutil= require('gulp-util');
-
+var coffee=require('gulp-coffee');
 
 //gulp.task('somenameofyourchoice', fn);
 
-gulp.task('log', function() {
-	gutil.log('Workflows are swell.');
+// gulp.task('log', function() {
+// 	gutil.log('Workflows are swell.');
+// });
+
+coffeeSources = ['components/coffee/tagline.coffee'];	//'components/coffee/*.coffee' okay also
+
+gulp.task('coffee', function() {
+	gulp.src(coffeeSources)
+		.pipe(coffee({bare:true})
+			.on('error', gutil.log))
+		.pipe(gulp.dest('components/scripts'))
 });
