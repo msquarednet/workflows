ab67c07af925c67c24ba917aabb06e3d9029d309
msquarednet <msquarednet@gmail.com>
2015-02-11 14:50:12 -0500

    Added Browserify task to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var gutil= require('gulp-util');
 var coffee=require('gulp-coffee');
 var concat=require('gulp-concat');
+var browserify = require('gulp-browserify');
 
 //gulp.task('somenameofyourchoicecalledfromterminal', fn);
 
@@ -29,6 +30,12 @@ gulp.task('js', function() {
   gulp.src(jsSources)
     //.pipe(sourcemaps.init())
       .pipe(concat('script.js'))
+      .pipe(browserify())
     //.pipe(sourcemaps.write())
     .pipe(gulp.dest('builds/development/js'));
 });
+
+
+
+
+
