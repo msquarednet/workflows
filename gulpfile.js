10045b443f51ff8e301f9f82e67ad97897c9a374
msquarednet <msquarednet@gmail.com>
2015-05-29 12:10:22 -0400

    Added Environment Variables

--- gulpfile.js
+++ gulpfile.js
@@ -4,26 +4,39 @@ var coffee 		= require('gulp-coffee');
 var concat 		= require('gulp-concat');
 var browserify    = require('gulp-browserify');
 var compass       = require('gulp-compass');
-var connect       = require('gulp-connect');
+var connect       = require('gulp-connect');    //no worky
+var livereload    = require('gulp-livereload'); //no worky, also
 
+var env, outputDir, sassStyle;
+var coffeeSources, jsSources, sassSources, htmlSources, jsonSources;
 
-//gulp.task('somenameofyourchoicecalledfromterminal', fn);
+env = process.env.NODE_ENV || 'development';
 
-// gulp.task('log', function() {
-// 	gutil.log('Workflows are swell.');
-// });
+if (env==='development') {
+  outputDir='builds/development/';
+  sassStyle='expanded';
+} else {
+  outputDir='builds/production/';
+  sassStyle='compressed';
+}
 
-var coffeeSources = ['components/coffee/tagline.coffee'];	//'components/coffee/*.coffee' okay also
+coffeeSources = ['components/coffee/tagline.coffee'];	//'components/coffee/*.coffee' okay also
 //var jsSources = 'components/scripts/*.js';
-var jsSources = [
+jsSources = [
 	'components/scripts/rclick.js',
 	'components/scripts/pixgrid.js',
 	'components/scripts/tagline.js',
 	'components/scripts/template.js'
 ];
-var sassSources = ['components/sass/style.scss']; 	//this is good, since sass has it's own import capability
-var htmlSources = ['builds/development/*.html'];
-var jsonSources = ['builds/development/js/*.json'];
+sassSources = ['components/sass/style.scss']; 	//this is good, since sass has it's own import capability
+htmlSources = [outputDir + '*.html'];
+jsonSources = [outputDir+'js/*.json'];
+
+
+//gulp.task('somenameofyourchoicecalledfromterminal', fn);
+// gulp.task('log', function() {
+//  gutil.log('Workflows are swell.');
+// });
 
 gulp.task('coffee', function() {
 	gulp.src(coffeeSources)
@@ -38,23 +51,27 @@ gulp.task('js', function() {
       .pipe(concat('script.js'))
       .pipe(browserify())
     //.pipe(sourcemaps.write())
-    .pipe(gulp.dest('builds/development/js'))
-    .pipe(connect.reload())
+    .pipe(gulp.dest(outputDir+'js'))
+    .pipe(livereload())//.pipe(connect.reload())
 });
 
 gulp.task('compass', function() {
   gulp.src(sassSources)
     .pipe(compass({	//no need for a config.rb file!!!
     	sass: 'components/sass',
-    	image: 'builds/development/images',
+    	image: outputDir+'images',
     	style: 'expanded'
     })
       .on('error', gutil.log))
-    .pipe(gulp.dest('builds/development/css'))
-    .pipe(connect.reload())
+    .pipe(gulp.dest(outputDir+'css'))
+    .pipe(livereload())//.pipe(connect.reload())
 });
 
 gulp.task('watch', function() {
+  livereload.listen({
+    start: true,
+    host: 'http://development.dev'
+  });
   gulp.watch(coffeeSources, ['coffee']);
   gulp.watch(jsSources, ['js']);
   gulp.watch('components/sass/*.scss', ['compass']);
@@ -66,21 +83,22 @@ gulp.task('connect', function() { //livereload doesn't work(?)
   connect.server({
     root: 'build/development/',
     livereload: true,
-    port: 8888,
+    //port: 8888,
     host: 'http://development.dev'
   });
 });
 
 gulp.task('html', function() {
   gulp.src(htmlSources)
-    .pipe(connect.reload())
+    .pipe(livereload())//.pipe(connect.reload())
 });
 gulp.task('json', function() {
   gulp.src(jsonSources)
-    .pipe(connect.reload())
+    .pipe(livereload())//.pipe(connect.reload())
 });
 
-gulp.task('default', ['html', 'json', 'coffee', 'js', 'compass', 'connect', 'watch']);  //dont NEED cb function
+// gulp.task('default', ['html', 'json', 'coffee', 'js', 'compass', 'connect', 'watch']);  //dont NEED cb function
+gulp.task('default', ['html', 'json', 'coffee', 'js', 'compass', 'watch']);  //dont NEED cb function
 
 
 
